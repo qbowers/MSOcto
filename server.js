59656cabec974612ff57ec78106aa054cb6db8af
Quinn Bowers <tmightyquinn@users.noreply.github.com>
2018-07-20 14:06:23 -0700

    killed setup page

--- server.js
+++ server.js
@@ -38,10 +38,6 @@ if (process.argv[2] && process.argv[2] == 'testweb') {
 
 
 
-
-
-
-
 //Assign each server a printer
 serial.refresh(system.testweb())
 .then(() => {
@@ -51,9 +47,11 @@ serial.refresh(system.testweb())
   //connect each server to a port
   //TODO: leave octoprint servers connected
   for (var i = 0; i < serial.length; i++) {
-    system.OctoPrints[i].disconnect();
+    console.log(serial[i]);
+
+    //system.OctoPrints[i].disconnect();
     //TODO: error handler: if the thing doesnt connect, try again a few times
-    system.OctoPrints[i].connect( serial[i] );
+    //system.OctoPrints[i].connect( serial[i] );
   }
 });
 
@@ -71,92 +69,6 @@ app.use(scss({
 }));
 //for reading the bodies of http posts
 app.use(upload.any());
-
-
-
-
-
-
-//set up
-app.get('/setup', (req, res) => {
-  res.render('setup.html', {
-    ready: system.ready(),
-    local: true,
-
-    Printers: system.Printers,
-    OctoPrints: system.OctoPrints
-  });
-});
-
-//gets a list of potential printers for a given server
-app.get('/setup/options/:server', (req, res) => {
-  var octoprint = system.OctoPrints[req.params.server];
-  var options = [];
-  for (var i = 0; i < system.Printers.length; i++) {
-    var printer = system.Printers[i];
-    if (printer.match(octoprint)) options.push(printer.name)
-  }
-
-  res.setHeader('Content-Type', 'application/json');
-  res.json(options);
-});
-
-//assign a printer to a server
-app.post('/setup/set', (req, res) => {
-
-  var printer,
-      octoprint = system.OctoPrints[parseInt(req.body.octoprint)];
-  //determine printer from printername
-  for (var i = 0; i < system.Printers.length; i++) if(system.Printers[i].name == req.body.printer) printer = system.Printers[i];
-
-
-  octoprint.wiggle.stop();
-  printer.attach(octoprint);
-
-
-
-  var ready = true;
-  for (var i = 0; i < serial.length && i < system.OctoPrints.length; i++) {
-    if (!system.OctoPrints[i].Printer) {
-      ready = false;
-      break;
-    }
-  }
-
-  if (ready) {
-    system.ready(true);
-    res.status(200);
-  } else res.status(204);
-
-  res.end();
-});
-
-//wiggle whatever printer is connected to a server
-app.post('/setup/wiggle', (req, res) => {
-  console.log('octoprint wiggled!');
-  var octoprint = system.OctoPrints[req.body.octoprint];
-  octoprint.wiggle.setTime(5);
-
-  res.status(204);
-  res.end();
-});
-
-
-
-
-//redirect to setup
-//lets through any scripty or style things
-app.use((req, res, next) => {
-  if ( system.ready() || req.path.includes('script/') || req.path.includes('style/') ) next();
-  else {
-    res.writeHead((req.method === 'GET') ? 302 : 310, {'Location': '/setup'});
-    res.end();
-  }
-});
-
-
-
-
 
 
 
