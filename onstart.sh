74ae45c66892f0541370731806509b883d45d969
Quinn Bowers <tmightyquinn@users.noreply.github.com>
2018-08-13 13:13:47 -0700

    port numbers

--- onstart.sh
+++ onstart.sh
@@ -21,23 +21,23 @@ parse_yaml() {
 echo "OctoStart has been run"
 
 cd /home/server/octo1
-./venv/bin/octoprint serve --port=5008  --config config.yaml >> log.txt &
+./venv/bin/octoprint serve --port=5001  --config config.yaml >> log.txt &
 cd /home/server/octo2
-./venv/bin/octoprint serve --port=5008  --config config.yaml >> log.txt &
+./venv/bin/octoprint serve --port=5002  --config config.yaml >> log.txt &
 cd /home/server/octo3
-./venv/bin/octoprint serve --port=5008  --config config.yaml >> log.txt &
+./venv/bin/octoprint serve --port=5003  --config config.yaml >> log.txt &
 cd /home/server/octo4
-./venv/bin/octoprint serve --port=5008  --config config.yaml >> log.txt &
+./venv/bin/octoprint serve --port=5004  --config config.yaml >> log.txt &
 cd /home/server/octo5
-./venv/bin/octoprint serve --port=5008  --config config.yaml >> log.txt &
+./venv/bin/octoprint serve --port=5005  --config config.yaml >> log.txt &
 cd /home/server/octo6
-./venv/bin/octoprint serve --port=5008  --config config.yaml >> log.txt &
+./venv/bin/octoprint serve --port=5006  --config config.yaml >> log.txt &
 cd /home/server/octo7
-./venv/bin/octoprint serve --port=5008  --config config.yaml >> log.txt &
+./venv/bin/octoprint serve --port=5007  --config config.yaml >> log.txt &
 cd /home/server/octo8
 ./venv/bin/octoprint serve --port=5008  --config config.yaml >> log.txt &
 
 #cd /home/server/MSOcto
-#node server.js 2>> log.txt
+#sudo node server.js 2>> log.txt
 
 echo "OctoStart has finished"
